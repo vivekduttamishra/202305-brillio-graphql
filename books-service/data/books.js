bf0d593eee46d93f845f16b7a6be4a9455cc55e3
Vivek Dutta Mishra <vivek@conceptarchitect.in>
2023-05-23 12:42:13 +0530

    day#5 update#1

---
+++ books-service/data/books.js
@@ -0,0 +1,51 @@
+var books = [
+    {
+        id: "the-accursed-god",
+        title: "The Accursed God",
+        author: "Vivek Dutta Mishra",
+        price: 299,
+        rating: 4.6,
+		description: "An Amazon best seller epic fiction based on Mahabharata"
+    },
+    {
+        id: "rashmirathi",
+        title: "Rashmirathi",
+        author: "Ramdhari Singh Dinkar",
+        price: 119,
+        rating: 4.8,
+		description: "A poetic tale of Karna of Mahabahrata"
+    },
+    {
+        id: "a-tale-of-two-cities",
+        title: "A Tale of Two Cities",
+        author: "Charles Dickens",
+        price: 151,
+        rating: 4.5,
+		description: "A classic period drama set in London and Paris"
+    },
+    {
+        id: "manas",
+        title: "Manas",
+        author: "Vivek Dutta Mishra",
+        price: 199,
+        rating: 4.5,
+		description: "A Poetic tale related to aftermath of Mahabharata"
+    },
+
+    {
+        id: "the-count-of-monteo-cristo",
+        title: "The Count of Monte Cristo",
+        author: "Alexandre Dumas",
+        price: 499,
+        rating: 4.9,
+		description: "Timeless classic tale"
+    },
+
+
+
+];
+
+//ES2015
+//export default books;
+
+module.exports=books; 
